8bb12b05ab531a2ef71f1f66605ca2034344a8b2
Suyash Abhishek Kumar <suyash.a.kumar@gmail.com>
2024-10-28 22:55:53 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import NavBar from './Components/NavBar/NavBar'
 import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
-import MyWork from './Components/MyWork/Mywork'
+import MyWork from './Components/MyWork/MyWork'
 import Contact from './Components/Contact/Contact'
 
 const App = () => {
