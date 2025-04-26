f4a119207a32ebeb0834311b3d934fa8c71f9650
Suyash Abhishek Kumar <166641239+Suyash-Abhishek-Kumar@users.noreply.github.com>
2025-04-26 12:18:15 +0530

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
         <h1><span>I am Suyash Abhishek Kumar,</span> a Game Devaloper based in India.</h1>
         <div className="hero-action">
             <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with Me</AnchorLink></div>
-            <div className="hero-resume"><a href="https://drive.google.com/file/d/11srXVOzrEOHD-JwBpMeTtjaukEhGANdn/view?usp=sharing" target="_blank" rel="noopener noreferrer">My Resume</a>
+            <div className="hero-resume"><a href="https://drive.google.com/file/d/188RpPBXjT66UUqeHBeqIUZes-ichakyO/view?usp=sharing" target="_blank" rel="noopener noreferrer">My Resume</a>
             </div>
         </div>
     </div>
